24957d04cdf78501bc0c5162db5871922ec531c5
bigfleet <jim@jimvanfleet.com>
2010-02-23 14:15:32 -0500

    Naive, but passing for boolean

--- lib/acts_as_eventable/event.rb
+++ lib/acts_as_eventable/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
   
   def self.attributes_from(eventable_options, key, old_val, new_val)
-    {:field_name => key}
+    msg = eventable_options[:events][key.to_sym][new_val]
+    {:field_name => key, :message => msg}
   end
 end
