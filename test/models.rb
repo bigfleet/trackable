cab547596ab7c3797de856646b5a7b997fd14437
bigfleet <jim@jimvanfleet.com>
2010-02-23 21:03:30 -0500

    Tests passing

--- test/models.rb
+++ test/models.rb
@@ -1,11 +1,11 @@
 class Foo < ActiveRecord::Base
-  acts_as_trackable :events =>{
+  trackable :events =>{
     :no_homers => {true => "Homers have been barred.", false => "Homers have been allowed."},
     :custom_status => {:message => Proc.new {|n| "The value of a custom string field changed to #{n}" }},
     :custom_bar_id => {:message => Proc.new{|n| "Active Bar set to #{n}"}}
   }
-  belongs_to :bar, :class_name => "Models::Bar"
-  belongs_to :custom_bar, :class_name => "Models::Bar"
+  belongs_to :bar, :class_name => "Bar"
+  belongs_to :custom_bar, :class_name => "Bar"
 end
 
 class Bar < ActiveRecord::Base
