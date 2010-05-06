80d1233ebbe862191d77d4075529b415dedf879b
Adam Hunter <adamhunter@tma1.com>
2010-05-06 16:55:08 -0400

    refactored attributes_from slightly

--- lib/trackable/event.rb
+++ lib/trackable/event.rb
@@ -1,25 +1,25 @@
 class Event < ActiveRecord::Base
   
   def self.attributes_from(model, key, old_val, new_val)
-    return if old_val.blank? && new_val.blank? #not something we need to track.
+    return if old_val.blank? && new_val.blank? # not something we need to track.
     eventable_options = model.class.eventable_options
-    return if [eventable_options[:exclude]].flatten.include?(key.to_sym)
-    reference = key[0..-4].to_sym if key.ends_with?("_id")
-    msg = if eventable_options[:events][key.to_sym] && eventable_options[:events][key.to_sym][new_val]
-      eventable_options[:events][key.to_sym][new_val]
-    elsif eventable_options[:events][key.to_sym] && eventable_options[:events][key.to_sym][:message]
-      the_proc = eventable_options[:events][key.to_sym][:message]
-      if key.ends_with?("_id") && model.class.reflect_on_association(reference) # hackish, but this is a convention
-        the_proc.call(model.send(reference).to_s)
-      else
-        the_proc.call(new_val)
-      end
-    elsif key.ends_with?("_id") && model.class.reflect_on_association(reference) # hackish, but this is a convention
+    return if Array(eventable_options[:exclude]).include?(key.to_sym)
+    
+    events       = eventable_options[:events]
+    attr_options = events[key.to_sym] if events
+    reference    = key.ends_with?("_id") ? key[0..-4].to_sym : key.to_sym
+    association  = model.class.reflect_on_association(reference)
+    
+    message = if attr_options && attr_options[new_val]
+      attr_options[new_val]
+    elsif attr_options && msg_proc = attr_options[:message]
+      association ? msg_proc.call(model.send(reference).to_s) : msg_proc.call(new_val)
+    elsif association
       "#{key.humanize} changed to #{model.send(reference).to_s}"
     else
       "#{key.humanize} changed to #{new_val}"
     end
-    {:field_name => key, :message => msg, :whodunnit => Trackable.whodunnit}
+    {:field_name => key, :message => message, :whodunnit => Trackable.whodunnit}
   end
   
 end
